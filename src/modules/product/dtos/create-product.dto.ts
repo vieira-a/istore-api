876caa549c7d526e7be34cbb65b76ea1ad43b7c1
Anderson Vieira <asvieira.br@gmail.com>
2023-12-24 17:56:26 -0300

    refactor: remove product status from CreateProductDto

--- src/modules/product/dtos/create-product.dto.ts
+++ src/modules/product/dtos/create-product.dto.ts
@@ -7,9 +7,6 @@ export class CreateProductDto {
   @IsNotEmpty({ message: 'Categoria do produto é obrigatório' })
   category: string;
 
-  @IsNotEmpty({ message: 'Status do produto é obrigatório' })
-  status: string;
-
   @IsNotEmpty({ message: 'Quantidade do produto é obrigatório' })
   quantity: number;
 
