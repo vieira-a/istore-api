0dd4d1533c2503a622959f46bdc487f6124c9906
Anderson Vieira <asvieira.br@gmail.com>
2023-12-24 16:39:37 -0300

    test: ensure CreateProductController returns 200 on success

---
+++ src/modules/product/__tests__/create-product.controller.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { registerSuccess } from '../../../modules/shared/helpers';
+import { productDataMock, productEntityMock } from '../__mocks__';
+import { CreateProductController } from '../controllers/create-product.controller';
+import { ProductEntity } from '../entities';
+import { CreateProductService } from '../services';
+
+describe('CreateProductController', () => {
+  let service: CreateProductService;
+  let controller: CreateProductController;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CreateProductController],
+      providers: [
+        CreateProductService,
+        {
+          provide: getRepositoryToken(ProductEntity),
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<CreateProductService>(CreateProductService);
+    controller = module.get<CreateProductController>(CreateProductController);
+  });
+
+  it('should return 200 on success', async () => {
+    jest.spyOn(service, 'create').mockResolvedValue(productEntityMock);
+
+    const result = await controller.create(productDataMock);
+    expect(result).toEqual(registerSuccess());
+  });
+});
