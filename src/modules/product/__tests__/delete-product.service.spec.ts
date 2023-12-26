4bb046020cd6d9cea7727054d038de5b44e0b0d8
Anderson Vieira <asvieira.br@gmail.com>
2023-12-26 16:05:09 -0300

    test: ensure DeleteProductService delete a product by id if it exists

--- src/modules/product/__tests__/delete-product.service.spec.ts
+++ src/modules/product/__tests__/delete-product.service.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DeleteProductService } from '../services';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import { productResolvedMock } from '../__mocks__';
 import { ProductEntity } from '../entities';
+import { DeleteProductService, ReadProductByIdService } from '../services';
 
 describe('DeleteProductService', () => {
+  let readProductByIdService: ReadProductByIdService;
   let service: DeleteProductService;
   let repository: Repository<ProductEntity>;
 
@@ -12,13 +15,23 @@ describe('DeleteProductService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DeleteProductService,
+        ReadProductByIdService,
         {
           provide: getRepositoryToken(ProductEntity),
           useClass: Repository,
         },
+        {
+          provide: ReadProductByIdService,
+          useValue: {
+            read: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
+    readProductByIdService = module.get<ReadProductByIdService>(
+      ReadProductByIdService,
+    );
     service = module.get<DeleteProductService>(DeleteProductService);
     repository = module.get<Repository<ProductEntity>>(
       getRepositoryToken(ProductEntity),
@@ -29,8 +42,11 @@ describe('DeleteProductService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should delete a product by id', async () => {
+  it('should delete a product by id if it exists', async () => {
     const productId = 1;
+    jest
+      .spyOn(readProductByIdService, 'read')
+      .mockResolvedValue(productResolvedMock);
     jest
       .spyOn(repository, 'delete')
       .mockResolvedValue({ raw: { effected: 1 } });
