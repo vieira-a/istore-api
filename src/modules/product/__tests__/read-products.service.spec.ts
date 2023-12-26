5055baceae109d79f54f54c1775dccb5b135c9e1
Anderson Vieira <asvieira.br@gmail.com>
2023-12-26 08:53:47 -0300

    test: ensure ReadProductsService returns all products on success

---
+++ src/modules/product/__tests__/read-products.service.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { ProductEntity } from '../entities';
+import { ReadProductsService } from '../services';
+import { productsResolvedMock } from '../__mocks__';
+
+describe('CreateProductService', () => {
+  let readProductService: ReadProductsService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [
+        ReadProductsService,
+        {
+          provide: getRepositoryToken(ProductEntity),
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    readProductService = app.get<ReadProductsService>(ReadProductsService);
+  });
+
+  it('should be defined', () => {
+    expect(readProductService).toBeDefined();
+  });
+
+  it('should returns all products on success', async () => {
+    jest
+      .spyOn(readProductService, 'read')
+      .mockResolvedValue([productsResolvedMock]);
+
+    const result = await readProductService.read();
+    expect(result).toEqual([productsResolvedMock]);
+  });
+});
