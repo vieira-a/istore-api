cfbdc108336dcd88fc495b6592799522d4458777
Anderson Vieira <asvieira.br@gmail.com>
2023-12-26 14:01:48 -0300

    test: ensure UpdateProductService returns 404 if not found product

--- src/modules/product/__tests__/update-product.service.spec.ts
+++ src/modules/product/__tests__/update-product.service.spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
-import { updateProductMock } from '../__mocks__';
+import { DataNotFoundException } from '../../../modules/shared/exceptions';
+import {
+  productMock,
+  productResolvedMock,
+  updateProductMock,
+} from '../__mocks__';
 import { ProductEntity } from '../entities';
-import { UpdateProductService } from '../services';
+import { ReadProductByIdService, UpdateProductService } from '../services';
 
 describe('UpdateProductService', () => {
   let updateProductService: UpdateProductService;
+  let readProductByIdService: ReadProductByIdService;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       providers: [
         UpdateProductService,
+        ReadProductByIdService,
         {
           provide: getRepositoryToken(ProductEntity),
           useValue: {
@@ -23,6 +30,10 @@ describe('UpdateProductService', () => {
 
     updateProductService =
       moduleRef.get<UpdateProductService>(UpdateProductService);
+
+    readProductByIdService = moduleRef.get<ReadProductByIdService>(
+      ReadProductByIdService,
+    );
   });
 
   it('should return an error if service throws', async () => {
@@ -35,7 +46,22 @@ describe('UpdateProductService', () => {
     expect(updateSpy).toThrow();
   });
 
+  it('should return 404 if product does not exist', async () => {
+    jest.spyOn(readProductByIdService, 'read').mockImplementation((id) => {
+      if (id === productResolvedMock.id) {
+        return Promise.resolve(productResolvedMock);
+      } else {
+        return Promise.resolve(null);
+      }
+    });
+
+    await expect(
+      updateProductService.update(11, productResolvedMock),
+    ).rejects.toThrow(DataNotFoundException);
+  });
+
   it('should update product on success', async () => {
+    jest.spyOn(readProductByIdService, 'read').mockResolvedValue(productMock);
     const result = await updateProductService.update(1, updateProductMock);
     expect(result).toEqual(updateProductMock);
   });
