1630cca427b68aaddbb850e5382ffae5c7fc4b25
Anderson Vieira <asvieira.br@gmail.com>
2023-12-26 14:01:57 -0300

    feat: ensure UpdateProductService returns 404 if not found product

--- src/modules/product/services/update-product.service.ts
+++ src/modules/product/services/update-product.service.ts
@@ -5,14 +5,22 @@ import { Repository } from 'typeorm';
 import { UpdateProductDto } from '../dtos';
 import { ProductEntity } from '../entities';
 import { UpdateProductUsecase } from '../interfaces';
+import { ReadProductByIdService } from './read-product-by-id.service';
+import { DataNotFoundException } from '../../../modules/shared/exceptions';
 
 @Injectable()
 export class UpdateProductService implements UpdateProductUsecase {
   constructor(
     @InjectRepository(ProductEntity)
     private readonly updateProductServiceRepository: Repository<ProductEntity>,
+    private readonly readProductByIdService: ReadProductByIdService,
   ) {}
   async update(productId: number, productData: UpdateProductDto) {
+    const productExists = await this.readProductByIdService.read(productId);
+    if (!productExists) {
+      throw new DataNotFoundException();
+    }
+
     return await this.updateProductServiceRepository.save({
       productId,
       ...productData,
