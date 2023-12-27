1eef91ff17005bf794b74f7428e4711692f07dec
Anderson Vieira <asvieira.br@gmail.com>
2023-12-27 13:21:06 -0300

    docs: config swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.setGlobalPrefix('api/v1');
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('istore-api-roducts')
+    .setVersion('1.0.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('api/v1/doc', app, swaggerDocument);
 
   await app.listen(3000);
 }
