dd634731d6b133f127aa9e2cbc34538273a59b38
Anderson Vieira <asvieira.br@gmail.com>
2023-12-27 13:56:30 -0300

    docs: add swagger documentation to read products feature

--- src/modules/product/controllers/read-products.controller.ts
+++ src/modules/product/controllers/read-products.controller.ts
@@ -3,11 +3,26 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { PageOptionsDto } from '../../../modules/shared/dtos';
 import { DataNotFoundException } from '../../../modules/shared/exceptions';
 import { ReadProductsService } from '../services';
+import {
+  ApiOkResponse,
+  ApiNotFoundResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
+import { ProductDto } from '../dtos';
 
 @Controller('products')
 export class ReadProductsController {
   constructor(private readonly readProductsService: ReadProductsService) {}
 
+  @ApiOkResponse({
+    type: [ProductDto],
+  })
+  @ApiNotFoundResponse({
+    description: 'Dados não encontrados: Produtos',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
   @Get()
   async read(@Query() pageOptionsDto: PageOptionsDto) {
     const result = await this.readProductsService.read(pageOptionsDto);
