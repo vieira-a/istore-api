d09c5f76724ccbea5089c8dfd00c5089ebe9cca0
Anderson Vieira <asvieira.br@gmail.com>
2023-12-27 13:49:14 -0300

    docs: add swagger documentation to delete product feature

--- src/modules/product/controllers/delete-product.controller.ts
+++ src/modules/product/controllers/delete-product.controller.ts
@@ -1,4 +1,9 @@
 import { Controller, Delete, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 
 import { deletedSuccess } from '../../../modules/shared/helpers';
 import { DeleteProductService } from '../services';
@@ -7,6 +12,15 @@ import { DeleteProductService } from '../services';
 export class DeleteProductController {
   constructor(private readonly deleteProductService: DeleteProductService) {}
 
+  @ApiOkResponse({
+    description: 'Excluído com sucesso',
+  })
+  @ApiNotFoundResponse({
+    description: 'Dados não encontrados: Produto',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
     const result = await this.deleteProductService.delete(id);
