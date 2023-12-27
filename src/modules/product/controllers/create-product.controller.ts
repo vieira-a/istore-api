b1806505aa8687a79b004d6c8d68c12c8f5b8802
Anderson Vieira <asvieira.br@gmail.com>
2023-12-27 13:39:28 -0300

    docs: add swagger documentation to create product feature

--- src/modules/product/controllers/create-product.controller.ts
+++ src/modules/product/controllers/create-product.controller.ts
@@ -3,10 +3,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { registerSuccess } from '../../../modules/shared/helpers';
 import { ProductDto } from '../dtos';
 import { CreateProductService } from '../services';
+import {
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
 
 @Controller('product/register')
 export class CreateProductController {
   constructor(private readonly createProductService: CreateProductService) {}
+
+  @ApiCreatedResponse({
+    description: 'Cadastrado com sucesso',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
   @Post()
   async create(@Body() productData: ProductDto) {
     const result = await this.createProductService.create(productData);
--- src/modules/product/dtos/create-product.dto.ts
+++ src/modules/product/dtos/create-product.dto.ts
@@ -1,13 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class ProductDto {
+  @ApiProperty({
+    example: 'Sabre de luz',
+    description: 'Nome descritivo para o produto',
+  })
   @IsNotEmpty({ message: 'Nome do produto é obrigatório' })
   name: string;
 
+  @ApiProperty({
+    example: 'Espadas',
+    description: 'Categoria do produto',
+  })
   @IsNotEmpty({ message: 'Categoria do produto é obrigatório' })
   category: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Quantidade do produto',
+  })
   @IsNotEmpty({ message: 'Quantidade do produto é obrigatório' })
   quantity: number;
 
