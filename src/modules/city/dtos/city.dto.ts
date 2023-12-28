071cd2a68102b4965a4f832c3db00f72dfb263fd
Anderson Vieira <asvieira.br@gmail.com>
2023-12-27 22:33:54 -0300

    docs: create FetchCityController documentation

--- src/modules/city/dtos/city.dto.ts
+++ src/modules/city/dtos/city.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CityDto {
+  @ApiProperty({
+    example: 3300100,
+    description: 'Código identificador da cidade',
+  })
   id: number;
+
+  @ApiProperty({
+    example: 'Angra dos Reis',
+    description: 'Nome da cidade',
+  })
   name: string;
 }
